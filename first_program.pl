e5c393ee174f5da9ac6d75b47f0e000577c2da6f
Jose Carpio <jose.carpio@gmail.com>
2019-03-13 13:56:27 +0100

    my first prolog program

---
+++ first_program.pl
@@ -0,0 +1,13 @@
+
+% This is my first Prolog Program
+
+% Logic
+% sun_sunning -> go_beach
+%      ^             ^
+%      |             |
+%  antecedent    consequent
+
+% Prolog
+go_beach :- sun_sunning.
+sun_sunning.
+
---
+++ first_program2.pl
@@ -0,0 +1,11 @@
+
+% This is my first Prolog Program
+
+% Logic
+% sun_sunning -> go_beach
+%      ^             ^
+%      |             |
+%  antecedent    consequent
+
+% Prolog
+go_beach :- sun_sunning.
