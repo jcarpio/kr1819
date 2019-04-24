0bed1e8a42a1a47b091642d4df4a6fd8d34db292
Jose Carpio <jose.carpio@gmail.com>
2019-04-24 14:27:47 +0200

    reverse

---
+++ reverse.pl
@@ -0,0 +1,22 @@
+% reverse(+List, -Result)
+% it is true if Result unify with a list
+% with the same elements that List have but
+% in reverse order.
+%
+% ? reverse([1,2,3,4], R).
+% R = [4, 3, 2, 1]
+%
+%
+
+
+myReverse([], []).
+
+myReverse([Head|Tail], R2):- myReverse(Tail, R),
+    append(R, [Head], R2).
+	
+% reverse([1,2,3,4],    ):- reverse([2,3,4], [4,3,2]), 
+
+% ? append([1,2,3], [4,5,6], R).
+% R = [1,2,3,4,5,6]
+
+    
